98129f67dfdc28954aa99017116456849bd8932c
Gabriel Gonçalves Stein <gabgstein@gmail.com>
2026-02-27 08:38:32 -0300

    fix color text - homepage

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
         <h3 className="text-[16px] text-blue-300 font-barlow-condensed md:text-[28px]">
           SO, YOU WANT TO TRAVEL TO
         </h3>
-        <h1 className="text-[80px] md:text-[100px] ">SPACE</h1>
+        <h1 className="text-[80px] md:text-[100px] text-white">SPACE</h1>
         <p className="text-[15px] text-blue-300 font-barlow md:text-[16px]">
           Let&apos;s face it; if you want to go to space, you might as well
           genuinely go to outer space and not hover kind of on the edge of it.
